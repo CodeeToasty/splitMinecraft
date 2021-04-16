641b17649544ad7b3d308dd4dfc3978597d0d160
CodeeToasty <61847758+CodeeToasty@users.noreply.github.com>
2021-04-16 21:51:01 +0200

    Add files via upload

---
+++ splitMinecraft.py
@@ -0,0 +1,39 @@
+#!/urs/bin/python3
+import sys
+
+def drawSolution(length, step, pillars):
+	i=0
+	print("L", end='')
+	while True:
+		for j in range(step):
+			print("#", end='')
+		if i == pillars:
+			break
+		print("@",end='')
+		i+=1
+	print("L")
+
+def checkSplit(length, step, p):
+	while True:
+		length-=step
+		if length<=0:
+			break
+		length-=1
+		p+=1
+	if length==0:
+		return True, p
+	return False, p
+
+def main(argv):
+	total=int(sys.argv[1])
+	
+	print("feasable solutions: ")
+	for step in range(2, int(total+1)):
+		supp_total=total
+		pillars=0
+		test,pillars = checkSplit(supp_total,step,pillars)
+		if test==True:
+			print(step, end=' ')
+			drawSolution(supp_total,step,pillars)
+
+main(sys.argv[1:])
