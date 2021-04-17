c47d6d55ba900c119685c11846a2a42227c698a0
Daniele Toschi <daniele@Whisperer.homenet.telecomitalia.it>
2021-04-17 13:41:12 +0200

    added setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+   name='splitMinecraft',
+   version='0.1',
+   description='Minecraft wall splitter',
+   author='Daniele Toschi',
+   author_email='tosted94@gmail.com',
+   packages=['splitMinecraft'],
+   install_requires=['numpy', 'matplotlib']
+)
